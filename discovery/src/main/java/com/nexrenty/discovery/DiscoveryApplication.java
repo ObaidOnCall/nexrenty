c6524ec726d3f4f6e245a51bb5da541431c33937
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-01-05 15:09:36 +0100

    Enable the discovery serivice

--- discovery/src/main/java/com/nexrenty/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/nexrenty/discovery/DiscoveryApplication.java
@@ -2,8 +2,11 @@ package com.nexrenty.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
